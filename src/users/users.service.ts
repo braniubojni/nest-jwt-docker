6de0f66a1402b8d380711020a2da7990674d3132
Erik <braniubojni@mail.ru>
2022-02-14 15:35:25 +0400

    service is ready for creation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { User } from "./users.model";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
+  async createUser() {}
+
+  async getAllUsers() {}
+}
