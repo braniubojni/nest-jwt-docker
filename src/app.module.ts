be45efc825cc881f40d7cafcf5bee1ce67d48ec0
Erik <braniubojni@mail.ru>
2022-02-14 15:12:48 +0400

    added production env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
-import { UsersModule } from './users/users.module';
+import { UsersModule } from "./users/users.module";
 
 @Module({
   controllers: [],
   providers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
       dialect: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "xiaomi",
-      database: "nest-course",
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
