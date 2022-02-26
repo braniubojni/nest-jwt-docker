86152e9acb8421f36484db60ca7f4eb7ac24ca19
Erik <braniubojni@mail.ru>
2022-02-26 22:42:38 +0400

    added login and registration functionality

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -15,6 +15,11 @@ export class JwtAuthGuard implements CanActivate {
     try {
       const authHeader = req.headers.authorization;
       const bearer = authHeader.split(" ")[0];
+      const token = authHeader.split(" ")[1];
+
+      if (bearer !== "Bearer" || !token) {
+        throw new UnauthorizedException({ message: "User is not authorized" });
+      }
     } catch (error) {
       throw new UnauthorizedException({ message: "User is not authorized" });
     }
